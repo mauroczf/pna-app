eb207ee7bdcd695a4df743a80f986a85af8c8f3e
mauroczf <mauroczf@gmail.com>
2021-10-20 10:39:41 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('v1')->group(function () {
+    Route::group(['middleware' => 'api'], function () {
+        Route::resource('users', 'API\UserController')->except(['create', 'show', 'edit']);
+        // essas rotas podem acessar a aplicação sem precisar de autenticação
+    });
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
